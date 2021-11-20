61f7f53a6d1ddcd21eb0959892f1cc6aaf6b2a68
Anna <storyteller.nemo@gmail.com>
2021-11-20 22:03:18 +0300

    program update

--- Program.cs
+++ Program.cs
@@ -31,19 +31,46 @@ namespace ITParkStuding
             }
         }
 
+      
         static void Main(string[] args)
         {
-            Farm farm = new Farm(20, 0);
-            farm.Farming();
-            Console.WriteLine();
+            //Farm farm = new Farm(20, 0);
+            //farm.Farming();
+            //Console.WriteLine();
 
-            WathingTower watchTow = new WathingTower(200);
-            watchTow.Alarm();
-            Console.WriteLine();
+            //WathingTower watchTow = new WathingTower(200);
+            //watchTow.Alarm();
+            //Console.WriteLine();
 
-            Castle castle = new Castle(200);
-            castle.Defence();
-            Console.WriteLine();
+            //Castle castle = new Castle(200);
+            //castle.Defence();
+            //Console.WriteLine();
+
+
+            DelegatesEvents ec = new DelegatesEvents();
+            ec.ValueChangedEvent += Method1;
+            ec.ValueChangedEvent += Method1;
+            ec.Number = 5;
+            Console.WriteLine(DelegatesEvents.taxRate);
+
+
+        }
+
+        static void Method1(int n)
+        {
+            Console.WriteLine(n);
+        }
+        static void Method2()
+        {
+            Console.WriteLine("value changedvalue changed");
+        }
+        static void Method3()
+        {
+            Console.WriteLine("value changedvalue changedvalue changed");
+        }
+        static void Method4()
+        {
+            Console.WriteLine("value changedvalue changedvalue changed");
         }
     }
 }
